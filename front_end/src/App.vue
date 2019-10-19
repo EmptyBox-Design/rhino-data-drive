cad46eb0053a3f3a8feb2716963ce8fdaacd8284
bpachuca <bpachuca@kpf.com>
2019-10-19 14:32:41 -0400

    init repo
    front-end folder contains vue-mapbox, bootstrap vue

---
+++ front_end/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+body, html{
+  margin:0;
+  padding:0;
+}
+// remove text-align center from root
+// https://stackoverflow.com/questions/46215197/mapbox-mapbox-gl-vue-renders-the-map-on-only-50-of-the-width-of-the-container
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ front_end/src/views/Map.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="map">
+    <MglMap :accessToken="accessToken" :mapStyle.sync="mapStyle" container="map-parent" @load="onMapLoaded" :zoom="zoom">
+      <MglNavigationControl position="top-right" />
+      <MglGeolocateControl position="top-right" />
+    </MglMap>
+  </div>
+</template>
+
+<script>
+// MAPBOX IMPORTS
+import Mapbox from "mapbox-gl";
+import { MglMap, MglNavigationControl, MglGeolocateControl, MglPopup, } from "vue-mapbox";
+
+export default {
+  name: 'home',
+  components: { MglMap, MglNavigationControl, MglGeolocateControl },
+  data(){
+    return {
+      accessToken: 
+      "pk.eyJ1IjoiZW1wdHlib3gtZGVzaWducyIsImEiOiJjanBtM3E3ajAwbDF0NDJsa2N0OWh4M3p0In0.ZhciPKsk9UUSjUN44kJrcw",
+      mapStyle: "mapbox://styles/emptybox-designs/cjpq0tslf1asv2srjg6zdwf6k",
+      stops: null,
+      zoom: 1
+    }
+  },
+  created(){
+    this.mapbox = Mapbox;
+  },
+  mounted(){
+  },
+  methods:{
+    onMapLoaded(event){
+      this.map = event.map
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#map{
+  width: 100vw;
+}
+#map-parent{
+  position: absolute;
+  height: calc(100vh - #{$navbar-height});
+
+  left: 0;
+  top: 0;
+
+  overflow: hidden;
+}
+  
+</style>
