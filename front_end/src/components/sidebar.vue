8e68045ca759cad8e101cd2a4b2ce6f064b2ed74
bpachuca <bpachuca@kpf.com>
2019-10-22 10:29:51 -0400

    Integrated export function to download button

--- front_end/src/components/sidebar.vue
+++ front_end/src/components/sidebar.vue
@@ -8,7 +8,8 @@
      <div class="col-12">
        <h5>Please Select Your Dataset</h5>
        <div class="active">
-         <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-0 w-100">
+         <label for="dropdown">American Community Survey Data</label>
+         <b-dropdown id="dropdown-1" text="Dropdown" class="m-0 w-100">
            <b-dropdown-item v-for="(dataset, index) in datasets" :key="index" @click="dataSelect(dataset.variable)">{{dataset.name}}</b-dropdown-item>
          </b-dropdown>
        </div>
@@ -23,43 +24,90 @@
 <script>
 import { mapState, mapGetters } from 'vuex'
 export default {
- name: "sidebar",
- data(){
-   return {
-   }
- },
- computed:{
-   ...mapGetters({
-     datasets: 'getdatasetNames',
-     datasetFlag: 'getDatasetFlag',
-     geojsonData: 'getDatasetBlockGroups'
-   })
- },
- methods:{
-   download(){
-    //  console.log("downloading dataset", );
-    let json = this.geojsonData
+  name: "sidebar",
+  data(){
+    return {
+    }
+  },
+  created(){
 
-    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json));
-    var dlAnchorElem = document.getElementById('downloadAnchorElem');
-    dlAnchorElem.setAttribute("href",dataStr);
-    dlAnchorElem.setAttribute("download", "download.geojson");
-    dlAnchorElem.click();
-   },
-   dataSelect(variable){
-     this.$store.dispatch("readACSDataURL", variable)
-   },
-   // fires whenever the address is updated in the $store
-   modelInputSubscriber: function () {
-     this.$store.subscribe((mutation, state) => {
-       switch (mutation.type) {
-         case 'setDatasetFlag':
-           this.$store.dispatch()
-           break
-       }
-     })
-   }
- }
+  },
+  computed:{
+    ...mapGetters({
+      datasets: 'getdatasetNames',
+      datasetFlag: 'getDatasetFlag',
+      geojsonData: 'getDatasetBlockGroups'
+    }),
+  },
+  methods:{
+    export3dm: (data) => {
+
+      let rhino = null
+      let file = null
+      
+      rhino3dm().then(function(library){
+
+        rhino = library
+        file = new rhino.File3dm();
+
+        data.forEach(function(d,i){
+          let points = new rhino.Point3dList();
+          let att = new rhino.ObjectAttributes();
+
+          console.log(d);
+
+          d.geometry.coordinates[0].forEach(function(j,k){
+            points.add(j[0], j[1], 0)
+          })
+
+          var curve = rhino.NurbsCurve.create(false,1, points);
+          let value = d.properties['dataValue'].toString()
+
+          att.setUserString('Household Income', value)
+          curve.setUserString('Household Income', value)
+
+          file.objects().addCurve(curve, att);
+        })
+
+        console.log(file.objects())
+
+        var base64 = file.toByteArray();
+
+        var binaryString = window.atob(base64);
+        var binaryLen = binaryString.length;
+        var bytes = new Uint8Array(binaryLen);
+
+        for (var i = 0; i < binaryLen; i++) {
+          var ascii = binaryString.charCodeAt(i);
+          bytes[i] = ascii;
+        }
+
+        var blob = new Blob([bytes], {type: "application/octet-stream"});
+        
+        var link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = 'model_download.3dm';
+        link.dispatchEvent( new MouseEvent( 'click' ) );
+      })
+    },
+    download(){
+      let json = this.geojsonData
+      this.export3dm(this.geojsonData.features);
+    },
+    dataSelect(variable){
+      this.$store.dispatch("readACSDataURL", variable)
+    },
+    // fires whenever the address is updated in the $store
+    modelInputSubscriber: function () {
+      this.$store.subscribe((mutation, state) => {
+        switch (mutation.type) {
+          case 'setDatasetFlag':
+            this.$store.dispatch()
+            break
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss">
